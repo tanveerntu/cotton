20b2e654f957e7bbf75ffaefd9235e2562512930
Tanveer Hussain <73167258+tanveerntu@users.noreply.github.com>
2022-01-31 09:33:12 +0500

    Update cotton.py

--- cotton.py
+++ cotton.py
@@ -497,8 +497,8 @@ fig.add_trace(go.Scatter(x=[df_h['Year'].iloc[-1]],
                          name='',
                          mode='markers+text',
                          marker=dict(color='red', size=14),
-                         textposition='top center',
-                         textfont=dict(family="fjalla one, sans-serif", color="#006BA2", size=24),
+                         textposition='top right',
+                         textfont=dict(family="fjalla one, sans-serif", color="#006BA2", size=20),
                          texttemplate='%{text:.3s}', #text shorten into 3 digits
                          showlegend=False))
 
@@ -510,7 +510,7 @@ fig.add_trace(go.Scatter(x=[df_h['Year'].iloc[57]],
                          mode='markers+text',
                          marker=dict(color='red', size=14),
                          textposition='middle right',
-                         textfont=dict(family="fjalla one, sans-serif", color="#006BA2", size=24),
+                         textfont=dict(family="fjalla one, sans-serif", color="#006BA2", size=20),
                          texttemplate='%{text:.3s}', #text shorten into 3 digits
                          showlegend=False))
 
